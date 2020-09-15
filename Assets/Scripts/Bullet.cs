f9f8141a4f5a41a9240f78275446f7550113f09f
redpelican2108 <xinwei2108@gmail.com>
2020-09-15 16:17:05 +0800

    Added Cannon and Bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed;
+    public Transform target;
+    public Transform cannon;
+    public float distance;
+    Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * bulletSpeed * Time.deltaTime;
+        if(Vector3.Distance(transform.position,cannon.position) > distance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    float counter;
+    public GameObject bullet;
+    public float timer;
+    public Transform targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(counter > 0)
+        {
+            counter -= Time.deltaTime;
+        }
+        else
+        {
+            GameObject projectile = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            projectile.GetComponent<Bullet>().target = targetPosition;
+            projectile.GetComponent<Bullet>().cannon = gameObject.transform;
+            counter = timer;
+        }
+    }
+}
